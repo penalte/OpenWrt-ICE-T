55ef651ab70b05f9026ce1c438e8d1e6c7c6676a
penalte <penaltebbv@hotmail.com>
2025-02-13 01:50:39 +0000

    Update configuration message for ISP Portugal Nos to remove test indication

--- src/lib/isps/isp_portugal_nos.sh
+++ src/lib/isps/isp_portugal_nos.sh
@@ -5,7 +5,7 @@ ISP_NAME="Nos"
 
 run_isp_portugal_nos() {
     banner
-    message "Configuring $ISP_NAME ($ISP_COUNTRY): test"
+    message "Configuring $ISP_NAME ($ISP_COUNTRY):"
     sleep 4
     additional_message="[INFO] $ISP_NAME ($ISP_COUNTRY): Configuration completed."
     display_main_menu  # Return to the main menu
